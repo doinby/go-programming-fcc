a54a6687bb7e39c704f4e03ec63136e77f1bfc2b
Chinh Do <chinh@doinby.co>
2023-08-18 20:32:12 +1000

    (0:27:39) Ch 2. Variables

--- main.go
+++ main.go
@@ -1,1 +1,51 @@
 package main
+
+func main() {
+
+	// 2.3. Type Inference
+	//penniesPerText := 2.0
+	//fmt.Printf("The type of penniesPerText is %T\n", penniesPerText)
+
+	// 2.4. Same Line Declaration
+	//averageOpenRate, displayMessage := .23, "is the average open rate of your message"
+	//fmt.Println(averageOpenRate, displayMessage)
+
+	// 2.5. Type Sizes
+	//accountAge := 2.6
+	//accountAgeInt := int(accountAge)
+	//fmt.Println("Your account has existed for", accountAgeInt, "years")
+
+	// 2.8. Constants
+	//const premiumPlanName = "Premium Plan"
+	//const basicPlanName = "Basic Plan"
+	//fmt.Println("plan:", premiumPlanName)
+	//fmt.Println("plan:", basicPlanName)
+
+	// 2.9. Computed Constants
+	//const secondsInMinute = 60
+	//const minutesInHour = 60
+	//const secondsInHour = secondsInMinute * minutesInHour
+	//fmt.Println("number of seconds in an hour:", secondsInHour)
+
+	// 2.10. Formatted String In Go
+	//const name = "Saul Goodman"
+	//const openRate = 30.5
+	//// Wrong solution - string const cannot have float value
+	////const msg = "Hi " + name + ", your open rate is " + string(openRate) + " percent"
+	//
+	//// Correct solution
+	//msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent", name, openRate)
+	//fmt.Println(msg)
+
+	// 2.11. Conditional
+	//messageLen := 10
+	//maxMessageLen := 20
+	//fmt.Println("Trying to send a message if length:", messageLen)
+	//if messageLen <= maxMessageLen {
+	//	fmt.Println("Message sent")
+	//} else {
+	//	fmt.Println("Message not sent")
+	//}
+
+	//
+}
